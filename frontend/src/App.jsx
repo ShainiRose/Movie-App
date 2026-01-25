77bd4d972b846c356e520c23afd49f1eeff348a6
shainiRose <shainiu22@gmail.com>
2026-01-25 09:40:29 +0530

    add conditional rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
-  
+  //conditional rendering
+  //const movieNumber = 1;
+ 
+
 
   return (
     <>
-     
-      <Text display="hii1"/>
-       <Text display="hii"/>
+                                      {/*
+                                    { movieNumber === 1 ? (
+                                      <MovieCard movie={{ title: "Tims Film", release_date: "2024" }} />
+                                    ) : (
+                                      <MovieCard movie={{ title: "Joes Film", release_date: "2020" }} />
+                                    )}
+                                    */}
+
+
+
+
     </>
   )
 }
-function Text({display}){
-  return (
-    <div>{display}</div>
-  )
-}
+
 export default App
+// curl brace = this is a varible [] = object 
+//Pass an empty JavaScript object as the movie prop to MovieCard , fist curly braces tells the this is a prop , second curl braces
+//is object literal in js
