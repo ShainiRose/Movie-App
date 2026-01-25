95f49cb503592c367d30916d22cb66758b914b92
shainiRose <shainiu22@gmail.com>
2026-01-25 09:20:26 +0530

    Initial commit - Movie App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <Text display="hii1"/>
+       <Text display="hii"/>
+    </>
+  )
+}
+function Text({display}){
+  return (
+    <div>{display}</div>
+  )
+}
+export default App
